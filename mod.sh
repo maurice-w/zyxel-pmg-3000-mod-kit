91fc53b4d3b9d57b82483c3807d4f5e834a1088a
Maurice Walker <maurice@walker.earth>
2026-08-04 18:38:36 +0200

    extract /etc/dropbear from old firmware

--- mod.sh
+++ mod.sh
@@ -2,6 +2,8 @@
 
 # FIRMWARE_IN needs to be a .upf file (with header), not a raw mtd2 / mtd3 image
 FIRMWARE_IN=$1
+# FIRMWARE_SSH_IN needs to be a an older firmware with SSH
+FIRMWARE_SSH_IN=$2
 FIRMWARE_OUT="./firmware_with_ssh.upf"
 FMK_DIR="/opt/firmware-mod-kit/trunk"
 FMK_EXTRACT="${FMK_DIR}/extract-firmware.sh"
@@ -24,10 +26,12 @@ function firmware_crc {
     echo -e "${crc}" | dd of=$FIRMWARE_OUT bs=1 seek=$1 count=4 conv=notrunc
 }
 
+$FMK_EXTRACT $FIRMWARE_SSH_IN
+cp -a ./fmk/rootfs/etc/dropbear ./
+rm -r ./fmk
+
 $FMK_EXTRACT $FIRMWARE_IN
 cp -a ./fmk/rootfs/usr/sbin/dropbear ./fmk/rootfs/usr/local/bin/
-# copy /etc/dropbear directory from firmware image with ssh first
-# cp -a ./fmk/rootfs/etc/dropbear ./
 cp -a ./dropbear ./fmk/rootfs/etc/
 $FMK_BUILD
 mv ./fmk/new-firmware.bin $FIRMWARE_OUT
